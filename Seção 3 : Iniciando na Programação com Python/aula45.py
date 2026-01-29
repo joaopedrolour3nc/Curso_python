1cdb3d18933ee141bad5b2ab802e2e5a7a29a978
João Pedro <joaopedro08moura@gmail.com>
2026-01-29 16:33:04 -0300

    Aula Sobre Iterador e mostrando o For por (dentro)

---
+++ Seção 3 : Iniciando na Programação com Python/aula45.py
@@ -0,0 +1,8 @@
+"""
+Iteravel -> str, range, etc (__iter__)
+Iterador -> Quem sabe entregar um valor por vez
+next -> Me entregue o próximo valor
+iter -> Me entregue seu iterador
+"""
+texto = 'Luiz'
+interator = iter(texto)
